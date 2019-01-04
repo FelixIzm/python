cde3f036ad819bcbdcef12f2a679e8c57db094bb
FelixIzm <for_cv@mail.ru>
2019-01-04 21:23:20 +0200

    test

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import requests
+import requests, re
 s=requests.get("https://obd-memorial.ru/html/search.htm?f=измайлов&n=&s=&y=&r=&p=1", allow_redirects=True)
 print(s.status_code)
 if(s.status_code==307):
@@ -7,6 +7,10 @@ if(s.status_code==307):
     print(s.cookies)
     print(s.cookies["3fbe47cd30daea60fc16041479413da2"])
     cookies = {'JSESSIONID': s.cookies["JSESSIONID"], "3fbe47cd30daea60fc16041479413da2":s.cookies["3fbe47cd30daea60fc16041479413da2"]}
-    s=requests.get("https://obd-memorial.ru/html/search.htm?f=измайлов&n=&s=&y=&r=&p=1", allow_redirects=True, cookies=cookies)
+    s=requests.get("https://obd-memorial.ru/html/search.htm?f=иванов&n=&s=&y=&r=&p=1", allow_redirects=True, cookies=cookies)
     print(s.cookies)
+    match = re.search(r'countPages = \d+',s.text,)
+    if match:
+        m1=re.search(r'\d+',match[0])
+        print(m1[0])
 
