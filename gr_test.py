f03de3d9daf649550dcad415e7194c7516dbae67
FelixIzm <for_cv@mail.ru>
2019-01-04 23:03:08 +0200

    test

--- gr_test.py
+++ gr_test.py
@@ -1,1 +1,5 @@
 from grab import Grab
+g  = Grab()
+resp = g.go('https://obd-memorial.ru/html/search.htm?f=измайлов&n=&s=&y=&r=&p=1')
+print(resp.code)
+print(resp.cookies.__getitem__('3fbe47cd30daea60fc16041479413da2'))
