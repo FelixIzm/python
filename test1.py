12eb8ee1eefaf7c7f071ad1e354b87a86f28a1d7
FelixIzm <for_cv@mail.ru>
2019-01-07 21:19:41 +0200

    ready

--- test1.py
+++ test1.py
@@ -1,34 +1,60 @@
 # -*- coding: utf-8 -*-
 import requests, re
-import asyncio
+import asyncio, random
 
-async def main():
-    URL = "https://obd-memorial.ru/html/search.htm?f=иванов&n=&s=&y=&r=&p=1"
-    loop = asyncio.get_event_loop()
+loop = asyncio.get_event_loop()
+URL = "https://obd-memorial.ru/html/search.htm?f=пеплов&n=&s=&y=&r="
+cookies = {}
+countPages = ""
+async def main(URL):
     future1 = loop.run_in_executor(None, requests.get, URL)
     s = await future1
-    print(s.status_code)
     if(s.status_code==307):
-        #print(s.cookies["JSESSIONID"])
-        #print(s.cookies)
-        #print(s.cookies["3fbe47cd30daea60fc16041479413da2"])
+        global cookies
         cookies = {'JSESSIONID': s.cookies["JSESSIONID"], "3fbe47cd30daea60fc16041479413da2":s.cookies["3fbe47cd30daea60fc16041479413da2"]}
         def do_req():
             return requests.get(URL,cookies=cookies)
         future2 = loop.run_in_executor(None, do_req )
         response = await future2
-
-        #s=requests.get(URL, cookies=cookies)
-        #print(s.cookies)
+        print('status_code_1={}'.format(response.status_code))
         match = re.search(r'countPages = \d+',response.text,)
         if match:
             m1=re.search(r'\d+',match[0])
             return(m1[0])
         else:
-            return 0 
+            return -1 
+    else:
+        print('status_code_2={}'.format(s.status_code))
+        match = re.search(r'countPages = \d+',s.text,)
+        if match:
+            m1=re.search(r'\d+',match[0])
+            return(m1[0])
+        else:
+            return -2
+
+countPages = loop.run_until_complete(main(URL))
+print(countPages)
+
+async def fetch_async(pid):
+    global URL
+    global cookies
+    URL = URL+"&p="+str(pid)
+    def do_req():
+        return requests.get(URL,cookies=cookies)
+    future1 = loop.run_in_executor(None, do_req )
+    response = await future1
+    cook = response.cookies.keys()
+    #print(cook)
+    #response.close()
+    return response.status_code
+
+async def asynchronous():
+    global countPages
+    futures = [fetch_async(i) for i in range(0, int(countPages)+1)]
+    for i, future in enumerate(asyncio.as_completed(futures)):
+        result = await future
+        print('{} {} '.format(i, result))
 
 
-loop = asyncio.get_event_loop()
-print(loop.run_until_complete(main()))
-
-
+loop.run_until_complete(asynchronous())
+loop.close()
