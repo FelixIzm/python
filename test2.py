6c7df7edb938988029c3dbdb432f79b5e643ef5a
FelixIzm <for_cv@mail.ru>
2019-01-08 22:05:24 +0200

    dd

---
+++ test2.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+import requests, re
+import asyncio, random
+
+secret_cookie=''
+loop = asyncio.get_event_loop()
+fxReq = '/search.htm?f=пеплов&n=&s=&y=&r='
+print(fxReq)
+URL = "https://obd-memorial.ru/html"+fxReq
+cookies = {}
+headers={}
+headers['Host']='obd-memorial.ru'
+headers['User-Agent']='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0'
+headers['Accept']='text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+headers['Accept-Language']='ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3'
+headers['Accept-Encoding']='gzip, deflate, br'
+headers['Connection']='keep-alive'
+headers['Upgrade-Insecure-Requests']='1'
+headers['Pragma']='no-cache'
+headers['Cache-Control']='no-cache'
+
+headers_req = headers.copy()
+countPages = ""
+async def main(URL):
+    global headers
+    future1 = loop.run_in_executor(None, requests.get, URL)
+    s = await future1
+    if(s.status_code==307):
+        global cookies
+        global secret_cookie
+        secret_cookie = s.cookies['3fbe47cd30daea60fc16041479413da2']
+        cookies = {'3fbe47cd30daea60fc16041479413da2':secret_cookie}
+        def do_req():
+            return requests.get(URL,cookies=cookies, headers = headers)
+        future2 = loop.run_in_executor(None, do_req )
+        response = await future2
+        print('status_code_1={}'.format(response.status_code))
+        match = re.search(r'countPages = \d+',response.text,)
+        if match:
+            m1=re.search(r'\d+',match[0])
+            return(m1[0])
+        else:
+            return -1 
+    else:
+        print('status_code_2={}'.format(s.status_code))
+        match = re.search(r'countPages = \d+',s.text,)
+        if match:
+            m1=re.search(r'\d+',match[0])
+            return(m1[0])
+        else:
+            return -2
+
+countPages = loop.run_until_complete(main(URL))
+print(countPages)
+
+async def fetch_async(pid):
+    global URL
+    global cookies
+    global secret_cookie
+    URL = URL+"&p="+str(pid)
+    headers_req['Cookie']='request=f%3D%D0%BF%D0%B5%D0%BF%D0%BB%D0%BE%D0%B2%26n%3D%26s%3D%26y%3D%26r%3D;3fbe47cd30daea60fc16041479413da2='+secret_cookie
+    def do_req():
+        return requests.get(URL,cookies=cookies,headers=headers_req)
+    future1 = loop.run_in_executor(None, do_req )
+    response = await future1
+    cook = response.cookies.keys()
+    print('{} {}'.format(response.status_code, response.cookies.keys()))
+    #response.close()
+    return response.cookies.keys()
+
+async def asynchronous():
+    global countPages
+    futures = [fetch_async(i) for i in range(0, int(countPages)+1)]
+    for i, future in enumerate(asyncio.as_completed(futures)):
+        result = await future
+        #print('{} {} '.format(i, result))
+
+
+loop.run_until_complete(asynchronous())
+loop.close()
+
