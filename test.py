9e90fac33b7a2e8aaee99a1e941bbe1db901a913
FelixIzm <for_cv@mail.ru>
2019-02-12 18:09:47 +0300

    dssf

--- test.py
+++ test.py
@@ -11,12 +11,15 @@ countPages = ''
 headers['User-Agent']='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0'
 loop = asyncio.get_event_loop()
 fio = 'пеплов'
+url1= 'https://obd-memorial.ru/html'
+url2 = '/search.htm?'
+url3= 'f='+fio+'&n=&s=&y=&r='
 def main(f):
     global JSESSIONID_value, secret_cookie,secret_cookie_value, countPages, headers, cookies
     URL = 'https://obd-memorial.ru/html'
     URL_search = URL + '/search.htm?f='+f+'&n=&s=&y=&r='
     s = requests.get(URL)
- 
+    print(s.status_code)
     if(s.status_code==307):
         secret_cookie_value = s.cookies[secret_cookie]
         cookies = { secret_cookie:secret_cookie_value}
@@ -42,7 +45,47 @@ print('secret cookie = '+secret_cookie_value)
 print('JSESSIONID = '+JSESSIONID_value)
 print('countPages = '+countPages)
 
+def get_page(page):
+    global cookies,headers
+    prev_search_ids = ''
+    if(page==1):
+        URL_search =url1+url2+url3
+        cookies[secret_cookie]=secret_cookie_value
+        cookies['request']=urllib.parse.quote(url3)
+        headers['cookie']=secret_cookie+"="+secret_cookie_value
+        res1 = requests.get(URL_search,cookies=cookies,headers=headers)
+        #print('JSESSIONID = '+str('JSESSIONID' in res1.cookies.keys()))
+        if('JSESSIONID' in res1.cookies.keys()):
+            cookies[secret_cookie]=secret_cookie_value
+            cookies['request']=urllib.parse.quote(url3)
+            cookies['JSESSIONID'] = JSESSIONID_value
+            res2 = requests.get(URL_search,cookies=cookies,headers=headers)
+            if('search_ids' in res2.cookies.keys()):
+                prev_search_ids = res2.cookies['search_ids']
+                #print('prev_search_ids = '+prev_search_ids)
+                #print(res2.cookies.keys())
+                return res2.cookies.keys()
+    else:
+        URL_search =URL_search =url1+url2+url3+'&p='+str(page)
+        cookies[secret_cookie]=secret_cookie_value
+        cookies['request']=urllib.parse.quote(url3)
+        cookies['JSESSIONID'] = JSESSIONID_value
+        cookies['ids'] = prev_search_ids
+        cookies['search_ids'] = prev_search_ids
+        #print(cookies)
+        res1 = requests.get(URL_search,cookies=cookies,headers=headers)
+        if(secret_cookie in res1.cookies.keys()):
+            res2 = requests.get(URL_search,cookies=cookies,headers=headers)
+            if('search_ids' in res2.cookies.keys()):
+                prev_search_ids = res2.cookies['search_ids']
+                #print(res2.cookies.keys())
+                return res2.cookies.keys()
+
+        
+
+
 async def fetch_async(pid, f):
+    print('page = '+str(pid))
     global cookies, headers,secret_cookie,secret_cookie_value
     URL_search ='https://obd-memorial.ru/html/search.htm?f='+fio+'&n=&s=&y=&r=&p='+str(pid)
     def do_req():
@@ -76,6 +119,11 @@ async def asynchronous():
         if ('search_ids' in result):
             print('{} {} '.format(i, result))
 
-loop.run_until_complete(asynchronous())
-loop.close()
+#loop.run_until_complete(asynchronous())
+#loop.close()
+futures = [get_page(i) for i in range(1, 6)]
+for i, future in enumerate(futures):
+    result = future
+    #if ('search_ids' in result):
+    print('{} {} '.format(i, result))
 
