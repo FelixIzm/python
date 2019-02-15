3a331da04bbd27f184e6514112215dc3efcf9712
FelixIzm <for_cv@mail.ru>
2019-02-15 19:16:21 +0200

    ura

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import requests, re
 import asyncio,urllib.parse
-
+from requests_html import HTMLSession
 cookies = {}
 headers={}
 secret_cookie = "3fbe47cd30daea60fc16041479413da2"
@@ -10,14 +10,14 @@ JSESSIONID_value = ''
 countPages = ''
 headers['User-Agent']='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0'
 loop = asyncio.get_event_loop()
-fio = 'измайлов'
+fio = 'пеплов'
 url1= 'https://obd-memorial.ru/html'
 url2 = '/search.htm?'
-url3= 'f='+fio+'&n=&s=&y=&r='
+url3= 'f='+fio+'&n=&s=&y=&r=&ps=100'
 def main(f):
     global JSESSIONID_value, secret_cookie,secret_cookie_value, countPages, headers, cookies
     URL = 'https://obd-memorial.ru/html'
-    URL_search = URL + '/search.htm?f='+f+'&n=&s=&y=&r='
+    URL_search = URL + '/search.htm?f='+f+'&n=&s=&y=&r=&ps=100'
     s = requests.get(URL)
     print(s.status_code)
     if(s.status_code==307):
@@ -47,14 +47,12 @@ print('countPages = '+countPages)
 ids=[]
 async def get_page(page):
     global cookies,headers,ids
-    #print('page = '+str(page))
     if(page==1):
         URL_search =url1+url2+url3
         cookies[secret_cookie]=secret_cookie_value
         cookies['request']=urllib.parse.quote(url3)
         headers['cookie']=secret_cookie+"="+secret_cookie_value
         res1 = requests.get(URL_search,cookies=cookies,headers=headers)
-        #print('JSESSIONID = '+str('JSESSIONID' in res1.cookies.keys()))
         if('JSESSIONID' in res1.cookies.keys()):
             cookies[secret_cookie]=secret_cookie_value
             cookies['request']=urllib.parse.quote(url3)
@@ -62,8 +60,6 @@ async def get_page(page):
             res2 = requests.get(URL_search,cookies=cookies,headers=headers)
             if('search_ids' in res2.cookies.keys()):
                 ids.append(res2.cookies['search_ids'])
-                #print('prev_search_ids = '+prev_search_ids)
-                #print(res2.cookies.keys())
                 return res2.cookies
             else:
                 while not ('search_ids' in res2.cookies.keys()):
@@ -77,19 +73,27 @@ async def get_page(page):
         cookies['JSESSIONID'] = JSESSIONID_value
         cookies['ids'] = ids[page-2]
         cookies['search_ids'] = ids[page-2]
-        #print(cookies)
         res1 = requests.get(URL_search,cookies=cookies,headers=headers)
         if(secret_cookie in res1.cookies.keys()):
             res2 = requests.get(URL_search,cookies=cookies,headers=headers)
             if('search_ids' in res2.cookies.keys()):
                 ids.append(res2.cookies['search_ids'])
-                #print(res2.cookies.keys())
                 return res2.cookies
             else:
                 while not ('search_ids' in res2.cookies.keys()):
                     res2 = requests.get(URL_search,cookies=cookies,headers=headers)
                 ids.append(res2.cookies['search_ids'])
                 return res2.cookies
+
+def get_info(id):
+    session = HTMLSession()
+    global cookies,headers
+    info_url ='https://obd-memorial.ru/html/info.htm?id='+id
+    res3 = session.get(info_url,cookies=cookies,headers=headers)
+    list_result = res3.html.find('.card_param-result')
+    for text_result in list_result:
+        print(text_result.text)
+
 
 
 
@@ -99,7 +103,10 @@ async def fxMain():
     for i, future in enumerate(futures):
         result = await future
         if ('search_ids' in result.keys()):
-            print('{} {} '.format(i, len(urllib.parse.unquote( result['search_ids']).split(' '))))
+            array_ids = urllib.parse.unquote( result['search_ids']).split(' ')
+            print('{} {} '.format(i, len(array_ids)))
+            for id in array_ids:
+                get_info(id)
 
 #fxMain()
 loop.run_until_complete(fxMain())
