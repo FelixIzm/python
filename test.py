9e7ba90326866e3e15c4cfb2361692f914eb966d
FelixIzm <for_cv@mail.ru>
2019-02-15 18:25:40 +0200

    ddfdds

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ JSESSIONID_value = ''
 countPages = ''
 headers['User-Agent']='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0'
 loop = asyncio.get_event_loop()
-fio = 'пеплов'
+fio = 'измайлов'
 url1= 'https://obd-memorial.ru/html'
 url2 = '/search.htm?'
 url3= 'f='+fio+'&n=&s=&y=&r='
@@ -91,49 +91,11 @@ async def get_page(page):
                 ids.append(res2.cookies['search_ids'])
                 return res2.cookies
 
-        
 
-
-async def fetch_async(pid, f):
-    print('page = '+str(pid))
-    global cookies, headers,secret_cookie,secret_cookie_value
-    URL_search ='https://obd-memorial.ru/html/search.htm?f='+fio+'&n=&s=&y=&r=&p='+str(pid)
-    def do_req():
-        return requests.get(URL_search,cookies=cookies,headers=headers)
-    #future1 = loop.run_in_executor(None, do_req )
-    #response = await future1
-    r1 = do_req()
-    #r=response.cookies.keys()
-    if('JSESSIONID' in r1.cookies.keys()):
-        JSESSIONID_value =  r1.cookies["JSESSIONID"]
-        cookies = {'JSESSIONID': JSESSIONID_value, secret_cookie:secret_cookie_value}
-        cookies['request'] = urllib.parse.quote('f=пеплов&n=&s=&y=&r=')
-        headers['JSESSIONID'] = JSESSIONID_value
-        headers['referer']='https://obd-memorial.ru/html/search.htm?f='+urllib.parse.quote('пеплов')+'&n=&s=&y=&r='
-
-        r2 = requests.get(URL_search,cookies=cookies,headers=headers)
-        #print(r2.cookies['search_ids'])
-    return r2.cookies.keys()
-
-
-#loop = asyncio.get_event_loop()
-#print(loop.run_until_complete(main()))
-
-async def asynchronous():
-    global countPages,fio
-    countPages_int = int(countPages)
-    #countPages_int=countPages_int+1
-    futures = [fetch_async(i, fio) for i in range(1, countPages_int)]
-    for i, future in enumerate(asyncio.as_completed(futures)):
-        result = await future
-        if ('search_ids' in result):
-            print('{} {} '.format(i, result))
-
-#loop.run_until_complete(asynchronous())
-#loop.close()
 
 async def fxMain():
-    futures = [get_page(i) for i in range(1, 7)]
+    global countPages
+    futures = [get_page(i) for i in range(1, int(countPages))]
     for i, future in enumerate(futures):
         result = await future
         if ('search_ids' in result.keys()):
