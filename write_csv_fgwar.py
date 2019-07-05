9060c5c76391e30e2b90d5ce95fe378fca62c144
FelixIzm <for_cv@mail.ru>
2019-07-05 21:56:33 +0300

    ass

--- write_csv_fgwar.py
+++ write_csv_fgwar.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import csv,json
 import sqlite3
-#conn = sqlite3.connect("./db/data.db") # или :memory: чтобы сохранить в RAM
-conn = sqlite3.connect("e:/temp/vpp/db/gwar.db") # или :memory: чтобы сохранить в RAM
+conn = sqlite3.connect("./db/data.db") # или :memory: чтобы сохранить в RAM
+#conn = sqlite3.connect("e:/temp/vpp/db/gwar.db") # или :memory: чтобы сохранить в RAM
 
-#csvfile = open('./csv/data.csv', 'w', newline='')
-csvfile = open('e:/temp/vpp/csv/data.csv', 'w', newline='')
+csvfile = open('./csv/data.csv', 'w', newline='')
+#csvfile = open('e:/temp/vpp/csv/data.csv', 'w', newline='')
 
 csv_columns = ['Фамилия']
 csv_columns.append('Имя')
@@ -20,6 +20,8 @@ csv_columns.append('Причина выбытия')
 csv_columns.append('Дата события')
 csv_columns.append('Место события')
 csv_columns.append('Тип документа')
+csv_columns.append('Архив')
+csv_columns.append('Дело')
 csv_columns.append('Ссылка на запись')
 
 writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
@@ -30,13 +32,18 @@ cursor.execute("SELECT count(1) FROM data")
 count_row = cursor.fetchone()[0]
 cursor.execute("SELECT * FROM data")
 for i in cursor.fetchall():
-    print(i[1])
     json.loads(i[1].replace("'",'"'))
-    #try:
-    #    writer.writerow(json.loads(i[1].replace("'",'"')))
-    #except:
-    #    print(i[1])
-    #    raise ValueError('Не определилось число страниц')
+    csv_str=''
+    for y,element in enumerate(eval(i[1])):
+        csv_str+='"'+csv_columns[y]+'":"'+element+'",'
+    csv_str="{"+csv_str[:-1]+"}"
+    print(csv_str)
+
+    try:
+        writer.writerow(json.loads(csv_str))
+    except:
+        print(csv_str)
+        raise ValueError('Ошибка записи строки')
 
     print ('{} из {}'.format(count,count_row))
     count+=1
