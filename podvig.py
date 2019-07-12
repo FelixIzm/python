39a1ff8600ed355433a674b9386f3545878147a0
FelixIzm <for_cv@mail.ru>
2019-07-12 09:20:43 +0300

    test

--- podvig.py
+++ podvig.py
@@ -2,9 +2,13 @@ from requests_html import HTMLSession
 import json
 
 session = HTMLSession()
-payload = {'json': '1', 'xmlParam': '<request firstRecordPosition="0" maxNumRecords="50" countResults="true"><record f23="P~одесская обл" entity="Человек Награждение"></record><record f23="P~одесская обл" entity="Человек Представление"></record><record f23="P~одесская обл" entity="Человек Картотека"></record><record f23="P~одесская обл" entity="Человек Юбилейная Картотека"></record></request>'}
+#stuff_in_string = f'Shepherd {shepherd} is {age} years old.'
+search_str = 'одесская обл'
+payload = f"{{'json': '1', 'xmlParam': '<request firstRecordPosition='0' maxNumRecords='50' countResults='true'><record f23='P~{search_str}' entity='Человек Награждение'></record><record f23='P~{search_str}' entity='Человек Представление'></record><record f23='P~{search_str}' entity='Человек Картотека'></record><record f23='P~{search_str}' entity='Человек Юбилейная Картотека'></record></request>'}}"
+#payload = f'string 'string' string' {{'json':'{search_str}'}} {search_str}'
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
 
-r = session.post("http://podvignaroda.ru/Image3/newsearchservlet", data=payload, headers=headers)
+r = session.post('http://podvignaroda.ru/Image3/newsearchservlet', data=json.dumps(payload.encode('utf-8')), headers=headers)
 #print(len(json.loads(r.text)['records']))
 print(r.text)
+#print(json.dumps(payload))
