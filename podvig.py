c9823fc41f8cd19358537cf271e6c1f6d126230e
FelixIzm <for_cv@mail.ru>
2019-07-14 18:03:47 +0300

    ddd

--- podvig.py
+++ podvig.py
@@ -1,17 +1,39 @@
 from requests_html import HTMLSession
-import json,pprint,asyncio, sys, sqlite3
+import json,pprint,asyncio, sys, sqlite3, argparse
 
 session = HTMLSession()
 loop = asyncio.get_event_loop()
 
+#######################
 search_str = 'херсонская обл'
-maxNumRecords = '5'
+maxNumRecords = '200'
+search_subject = 'f8' # f8 - призыв, f23 - место рождения
+#######################
 firstRecordPosition = str(0)
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
 
 #conn = sqlite3.connect('./db/all_fields.db') # или :memory: чтобы сохранить в RAM
 conn = sqlite3.connect('E:\\Temp\\db\\all_fields.db') # или :memory: чтобы сохранить в RAM
 cursor = conn.cursor()
+
+
+
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('name', nargs='?')
+    return parser
+ 
+ 
+if __name__ == '__main__':
+    parser = createParser()
+    namespace = parser.parse_args()
+    print(namespace)
+    # если что-то передали в аргументах, удаляем таблицы и все заново
+    if namespace.name:
+        cursor.execute("DROP TABLE if exists search_ids")
+        cursor.execute("DROP TABLE if exists pages")
+
+
 
 # Создание таблицы
 
@@ -45,8 +67,8 @@ f_struct['f23']='Место рождения'
 
 
 async def get_page(firstRecordPosition):
-    global maxNumRecords, search_str
-    xmlParam = f'<request firstRecordPosition="{firstRecordPosition}" maxNumRecords="{maxNumRecords}" countResults="true"><record f23="P~{search_str}" entity="Человек Награждение"></record><record f23="P~{search_str}" entity="Человек Представление"></record><record f23="P~{search_str}" entity="Человек Картотека"></record><record f23="P~{search_str}" entity="Человек Юбилейная Картотека"></record></request>'
+    global maxNumRecords, search_str, search_subject
+    xmlParam = f'<request firstRecordPosition="{firstRecordPosition}" maxNumRecords="{maxNumRecords}" countResults="true"><record {search_subject}="P~{search_str}" entity="Человек Награждение"></record><record {search_subject}="P~{search_str}" entity="Человек Представление"></record><record {search_subject}="P~{search_str}" entity="Человек Картотека"></record><record {search_subject}="P~{search_str}" entity="Человек Юбилейная Картотека"></record></request>'
     payload = {'json': '1'}
     payload['xmlParam']=xmlParam
     #print(xmlParam)
@@ -59,10 +81,10 @@ async def get_page(firstRecordPosition):
         return 'not ok'
 
 async def fxMain():
-    global headers,countPages, totalRecords, search_str,maxNumRecords,insertedPages
+    global headers,countPages, totalRecords, search_str,maxNumRecords,insertedPages, search_subject
     maxNumRecordsLocal = '10'
     firstRecordPosition = str(0)
-    xmlParam = f'<request firstRecordPosition="{firstRecordPosition}" maxNumRecords="{maxNumRecordsLocal}" countResults="true"><record f23="P~{search_str}" entity="Человек Награждение"></record><record f23="P~{search_str}" entity="Человек Представление"></record><record f23="P~{search_str}" entity="Человек Картотека"></record><record f23="P~{search_str}" entity="Человек Юбилейная Картотека"></record></request>'
+    xmlParam = f'<request firstRecordPosition="{firstRecordPosition}" maxNumRecords="{maxNumRecordsLocal}" countResults="true"><record {search_subject}="P~{search_str}" entity="Человек Награждение"></record><record {search_subject}="P~{search_str}" entity="Человек Представление"></record><record {search_subject}="P~{search_str}" entity="Человек Картотека"></record><record {search_subject}="P~{search_str}" entity="Человек Юбилейная Картотека"></record></request>'
     payload = {'json': '1'}
     payload['xmlParam']=xmlParam
     r = session.post("http://podvignaroda.ru/Image3/newsearchservlet", data=payload, headers=headers)
@@ -85,15 +107,11 @@ async def fxMain():
                     cursor.execute('insert into search_ids(id,flag,csv) values ('+rec['id']+',1,"'+str(db_json).replace('"',"")+'")')
                     conn.commit()
 
-                print ('{} из {}'.format(str(insertedPages+i),str(totalRecords)))
+                print ('{} из {}'.format(str(insertedPages+i+1),str(countPages)))
                 cursor.execute('update pages set num='+str(i+insertedPages+1))
                 conn.commit()
-
-
-
-
     else:
-        print('Not OK')
+        print('Result Not OK')
 
 loop.run_until_complete(fxMain())
 loop.close()
---
+++ read_excel.py
@@ -0,0 +1,20 @@
+import xlrd 
+
+#import pandas as pd
+#from pandas import ExcelFile
+
+loc = ("E:\\Южное Кладбище\\VGD\\csv\\Нерльский_рн.xlsx") 
+
+#df = pd.read_excel(loc, sheetname='data')
+#print("Column headings:")
+#print(df.columns)
+
+
+wb = xlrd.open_workbook(loc) 
+sheet = wb.sheet_by_index(0) 
+# For row 0 and column 0 
+sheet.cell_value(0, 0) 
+# Extracting number of columns 
+#for i in range(sheet.nrows):
+for i in range(5):
+    print(sheet.row(i)[0].value)
