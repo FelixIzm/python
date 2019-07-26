00b43bfa099aed3e5bfe8411460198e2924df60f
FelixIzm <for_cv@mail.ru>
2019-07-26 12:20:24 +0300

    ssdff

--- read_excel.py
+++ read_excel.py
@@ -4,11 +4,7 @@ import xlrd
 #from pandas import ExcelFile
 
 #loc = ("E:\\Южное Кладбище\\VGD\\csv\\Нерльский_рн.xlsx") 
-<<<<<<< HEAD
-loc = ("E:\\Южное Кладбище\\VGD\\csv\\Московская_ПодвигНарода_Призыв.xlsx")
-=======
 loc = ("C:\\BackUp\\Docs\\Южное Кладбище\\Выгрузки\\москворецкий_рвк.xlsx") 
->>>>>>> 8d673ced1baf93a5857a61cc7b1adc73ac9a6dc3
 
 #df = pd.read_excel(loc, sheetname='data')
 #print("Column headings:")
