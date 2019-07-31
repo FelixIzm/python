d9a292a972da308735bec6bb72159a3bb09e094b
FelixIzm <for_cv@mail.ru>
2019-07-31 17:48:16 +0300

    ssss

--- first.py
+++ first.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime, re
 a = ['one','','three']
 b={'id':'12345678','fl':a}
 print(len(a))
@@ -6,3 +6,5 @@ print(b)
 print(b['id'])
 today = datetime.datetime.today()
 print( today.strftime("%d-%m-%Y %H.%M.%S") )
+str1 = '50574334'
+print(re.findall(r'\d+', str1)[0])
--- s_mongo_test.py
+++ s_mongo_test.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-import pymongo, json, xlrd, sys, pprint, datetime,os
+import pymongo, json, xlrd, sys, pprint, datetime,os,re
 from datetime import datetime
 from os import listdir
 from os.path import isfile, join
@@ -10,7 +10,7 @@ client = MongoClient('35.193.230.58',
     authSource='test',authMechanism='SCRAM-SHA-1')
 
 db = client['test']
-collection_name = 'short_field'
+collection_name = 'data'
 if collection_name not in db.list_collection_names():
     db.create_collection(collection_name)
     db[collection_name].create_index("id", unique=True)
@@ -33,8 +33,8 @@ file_names_collection = db[file_names]
 fx_rec = '''{{"f0": {0},"id": {1},"fl":["{2}","{3}","{4}","{5}","{6}","{7}","{8}","{9}","{10}","{11}"]}}'''
 
 # Собираем названия файлов для обработки
-mypath="E:\\Temp\\obd\\"
-#mypath="c:\\Temp\\obd\\"
+#mypath="E:\\Temp\\obd\\"
+mypath="c:\\Temp\\obd\\"
 
 onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath, f))]
 
@@ -53,7 +53,7 @@ def write_json(floc):
                     if(str(row[0].value).isdigit()):
                         #print(row[0].value)
                         try:
-                            ss = fx_rec.format('"обд"', int(row[0].value), row[2].value, row[3].value, row[4].value, str(row[5].value).replace('"','').replace('\\','').replace('\n',''), str(row[6].value).replace('"','').replace('\\','').replace('\n',''), str(row[7].value).replace('"','').replace('\\','').replace('\n',''),str(row[8].value).replace('"','').replace('\\','').replace('\n',''), row[9].value, row[10].value,row[11].value)
+                            ss = fx_rec.format('"обд"', int(row[0].value), row[2].value.replace('\r','').replace('\n',' '), row[3].value, row[4].value, str(row[5].value).replace('"','').replace('\\','').replace('\n',''), str(row[6].value).replace('"','').replace('\\','').replace('\n',''), str(row[7].value).replace('"','').replace('\\','').replace('\n',''),str(row[8].value).replace('"','').replace('\\','').replace('\n',''), row[9].value, row[10].value,row[11].value)
                             a_rec.append(json.loads(ss))
                         except Exception as ex: 
                             template = "Тип ошибки {0} \nArguments:{1!r}"
@@ -66,25 +66,30 @@ def write_json(floc):
                 except pymongo.errors.BulkWriteError as e:
                     print('writeErrors')
                 # Подсчитываем затраченное время
-
-    #print(' id count = {0}'.format(len(a_rec)))
-    #wb.release_resources()
-    #del wb
-    #return
     else:
         sheet = wb.sheet_by_index(0) 
         obj = sheet.get_rows()
-        obj.__next__()
+        #obj.__next__()
         # Собираем данные из таблицы в json
-        for i, row in enumerate(obj):
-            try:
-                a_rec.append(json.loads(fx_rec.format('"обд"', int(row[0].value), row[1].value, row[2].value, row[3].value, row[4].value, str(row[5].value).replace('"','').replace('\\',''), str(row[6].value).replace('"','').replace('\\',''), str(row[7].value).replace('"','').replace('\\',''),row[8].value, row[9].value, row[10].value)))
-            except Exception as ex: 
-                template = "Тип ошибки {0} \nArguments:{1!r}"
-                message = template.format(type(ex).__name__, ex.args)
-                print(message)
-                print(row)
-                sys.exit(0)
+        for i,row in enumerate(sheet.get_rows()):
+            if(row[0].ctype==0):
+                continue
+            if(row[0].ctype==1):
+                str1=re.findall(r'\d+', row[0].value)[0]
+            else:
+                str1 = str(row[0].value)
+            print(str1)
+            if(str1.isnumeric()):
+                if i % 1000 == 0:
+                    print(i)
+                try:
+                    a_rec.append(json.loads(fx_rec.format('"обд"', int(str1), row[1].value, row[2].value, row[3].value, row[4].value, str(row[5].value).replace('"','').replace('\\',''), str(row[6].value).replace('"','').replace('\\',''), str(row[7].value).replace('"','').replace('\\',''),row[8].value, row[9].value, row[10].value).replace('\r','').replace('\n',' ')))
+                except Exception as ex: 
+                    template = "Тип ошибки {0} \nArguments:{1!r}"
+                    message = template.format(type(ex).__name__, ex.args)
+                    print(message)
+                    print(row)
+                    sys.exit(0)
         # Пишем в коллекцию МонгоДБ
         try:
             records.insert_many(a_rec, ordered=False)
@@ -100,6 +105,6 @@ def write_json(floc):
 for ff in onlyfiles:
     if(file_names_collection.find({"file":ff, "size":os.path.getsize(join(mypath,ff))}).count()==0):
         write_json(ff)
-        today = datetime.today()
-        file_name = '{{"file":"{0}", "date":"{1}", "size":{2}}}'.format(ff, str(today.strftime("%d-%m-%Y %H.%M.%S")),os.path.getsize(join(mypath,ff))).encode('utf-8')
-        file_names_collection.insert_one(json.loads(file_name))
+        #today = datetime.today()
+        #file_name = '{{"file":"{0}", "date":"{1}", "size":{2}}}'.format(ff, str(today.strftime("%d-%m-%Y %H.%M.%S")),os.path.getsize(join(mypath,ff))).encode('utf-8')
+        #file_names_collection.insert_one(json.loads(file_name))
