f31ca6ff6062123cbaa1078a1c840da9c684a9fe
FelixIzm <for_cv@mail.ru>
2019-08-02 04:49:23 +0300

    fff

--- s_mongo_test.py
+++ s_mongo_test.py
@@ -33,8 +33,8 @@ file_names_collection = db[file_names]
 fx_rec = '''{{"f0": {0},"id": {1},"fl":["{2}","{3}","{4}","{5}","{6}","{7}","{8}","{9}","{10}","{11}"]}}'''
 
 # Собираем названия файлов для обработки
-#mypath="E:\\Temp\\obd\\"
-mypath="c:\\Temp\\obd\\"
+mypath="E:\\Temp\\obd\\"
+#mypath="c:\\Temp\\obd\\"
 
 onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath, f))]
 
@@ -49,11 +49,18 @@ def write_json(floc):
         for sheet in wb.sheets():
             print('\tимя={0} строк {1}'.format(sheet.name,sheet.nrows))
             if(sheet.nrows > 4):
-                for row in sheet.get_rows():
-                    if(str(row[0].value).isdigit()):
-                        #print(row[0].value)
+                for i, row in enumerate(sheet.get_rows()):
+                    if(row[0].ctype==0):
+                        continue
+                    if(row[0].ctype==1 and row[0].value!='id'):
+                        str1=re.findall(r'\d+', row[0].value)[0]
+                    else:
+                        str1 = str(row[0].value)
+                    if(str1.isnumeric()):
+                        if i % 1000 == 0:
+                            print(i)
                         try:
-                            ss = fx_rec.format('"обд"', int(row[0].value), row[2].value.replace('\r','').replace('\n',' '), row[3].value, row[4].value, str(row[5].value).replace('"','').replace('\\','').replace('\n',''), str(row[6].value).replace('"','').replace('\\','').replace('\n',''), str(row[7].value).replace('"','').replace('\\','').replace('\n',''),str(row[8].value).replace('"','').replace('\\','').replace('\n',''), row[9].value, row[10].value,row[11].value)
+                            ss = fx_rec.format('"обд"', int(str1), row[2].value.replace('\r','').replace('\n',' '), row[3].value, row[4].value, str(row[5].value).replace('"','').replace('\\','').replace('\n',''), str(row[6].value).replace('"','').replace('\\','').replace('\n',''), str(row[7].value).replace('"','').replace('\\','').replace('\n',''),str(row[8].value).replace('"','').replace('\\','').replace('\n',''), row[9].value, row[10].value,row[11].value)
                             a_rec.append(json.loads(ss))
                         except Exception as ex: 
                             template = "Тип ошибки {0} \nArguments:{1!r}"
@@ -68,20 +75,19 @@ def write_json(floc):
                 # Подсчитываем затраченное время
     else:
         sheet = wb.sheet_by_index(0) 
-        obj = sheet.get_rows()
+        #obj = sheet.get_rows()
         #obj.__next__()
         # Собираем данные из таблицы в json
         for i,row in enumerate(sheet.get_rows()):
             if(row[0].ctype==0):
                 continue
-            if(row[0].ctype==1):
+            if(row[0].ctype==1 and row[0].value!='id'):
                 str1=re.findall(r'\d+', row[0].value)[0]
             else:
                 str1 = str(row[0].value)
-            print(str1)
             if(str1.isnumeric()):
                 if i % 1000 == 0:
-                    print(i)
+                    print('{0} id={1}').format(i,row[0].value)
                 try:
                     a_rec.append(json.loads(fx_rec.format('"обд"', int(str1), row[1].value, row[2].value, row[3].value, row[4].value, str(row[5].value).replace('"','').replace('\\',''), str(row[6].value).replace('"','').replace('\\',''), str(row[7].value).replace('"','').replace('\\',''),row[8].value, row[9].value, row[10].value).replace('\r','').replace('\n',' ')))
                 except Exception as ex: 
@@ -105,6 +111,6 @@ def write_json(floc):
 for ff in onlyfiles:
     if(file_names_collection.find({"file":ff, "size":os.path.getsize(join(mypath,ff))}).count()==0):
         write_json(ff)
-        #today = datetime.today()
-        #file_name = '{{"file":"{0}", "date":"{1}", "size":{2}}}'.format(ff, str(today.strftime("%d-%m-%Y %H.%M.%S")),os.path.getsize(join(mypath,ff))).encode('utf-8')
-        #file_names_collection.insert_one(json.loads(file_name))
+        today = datetime.today()
+        file_name = '{{"file":"{0}", "date":"{1}", "size":{2}}}'.format(ff, str(today.strftime("%d-%m-%Y %H.%M.%S")),os.path.getsize(join(mypath,ff))).encode('utf-8')
+        file_names_collection.insert_one(json.loads(file_name))
